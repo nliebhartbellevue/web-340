142c769847dc9f3d9c352a55c6d302534054c8d4
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2019-09-23 19:28:28 -0500

    updated

--- ems/app.js
+++ ems/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
 
