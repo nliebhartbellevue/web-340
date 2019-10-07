9f58b5946f228a2f29b78f6ab00509e3d28d26d3
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2019-10-07 11:03:36 -0500

    updated routes

--- ems/app.js
+++ ems/app.js
@@ -4,37 +4,33 @@
  * Date: September 24, 2019
  * Description: Server file for ems application
  */
-
+// Require statements
 const express = require("express");
+const http = require("http");
 const path = require("path");
+const logger = require("morgan");
+const helmet = require("helmet");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const csrf = require("csurf");
-const logger = require("morgan");
-const helmet = require("helmet");
 const mongoose = require("mongoose");
-const expressValidator = require("express-validator");
-const flash = require("connect-flash");
-const session = require("express-session");
-const passport = require("passport");
 const config = require("./config/database");
+const Employee = require("./models/employee");
 
-// Connect to MongoDB by way of Mongoose
-mongoose.connect(config.database, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+/**
+ * Connect to mLab using config file
+ */
+const mongoDB = config.database;
+mongoose.connect(mongoDB, {
+  useMongoClient: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true
 });
-
-let db = mongoose.connection;
-
-// Check connection to mLab
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => {
-  console.log("Connected to MongoDB cia mLab!");
-});
-
-// Check for database errors
-db.on("error", err => {
-  console.error(err);
+  console.log("EMS Server connected to mLab");
 });
 
 // Set-up CSRF protection
@@ -43,142 +39,70 @@ let csrfProtection = csrf({ cookie: true });
 // Init Express App
 const app = express();
 
-let Employee = require("./models/employee");
-
 /**
- * Body Parser Middleware
- * parse application/x-www-form-urlencoded
+ * Configure middleware and dependency libraries
  */
-app.use(bodyParser.urlencoded({ extended: true }));
-// parse application/json
-app.use(bodyParser.json());
 
-// Cookie Parser middleware
+// Morgan logger
+app.use(logger("short"));
+
+// Body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Cookie parser
 app.use(cookieParser());
 
-// Helmet middleware
+// Helmet
 app.use(helmet.xssFilter());
 
-// CSRF protection middleware
+// CSRF protection
 app.use(csrfProtection);
 
-// Inspect all incoming request and add a CSRF token to the response
+/**
+ * Set up CSRF protection to intercept all incoming request
+ * and add a CSRF token to the response
+ */
 app.use((req, res, next) => {
-  var token = req.csrfToken();
+  let token = req.csrfToken();
   res.cookie("XSRF-TOKEN", token);
   res.locals.csrfToken = token;
   next();
 });
 
-// Set Express to get views from views folder and use ejs as view engine
-app.set("views", path.join(__dirname, "views"));
+/**
+ * Set up view engine
+ * view's directory path
+ * and server port
+ */
+app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
-// Tell Express to serve static assets from the public folder
-app.use(express.static(path.join(__dirname, "public")));
-
-// Morgan middleware
-app.use(logger("short"));
-
-// Express Session middleware
-app.use(
-  session({
-    secret: config.secret,
-    resave: true,
-    saveUninitialized: true
-  })
-);
-
-// Express Messages middleware
-app.use(require("connect-flash")());
-app.use((req, res, next) => {
-  res.locals.messages = require("express-messages")(req, res);
-  next();
-});
-
-// Express Validator middleware
-app.use(
-  expressValidator({
-    errorFormatter: (param, msg, value) => {
-      var namespace = param.split("."),
-        root = namespace.shift();
-      formParam = root;
-
-      while (namespace.length) {
-        formParam += "[" + namespace.shift() + "]";
-      }
-      return {
-        param: formParam,
-        msg,
-        value
-      };
-    }
-  })
-);
-
-// Set PORT variable
 app.set("port", process.env.PORT || 8080);
-
-// Passport Config
-require("./config/passport")(passport);
-// Passport Middleware
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Auth guard route
-app.get("*", (req, res, next) => {
-  res.locals.user = req.user || null;
-  next();
-});
 
 /**
  * Description: Redirects users to Home page
  * Type: GET
  * Request: n/a
- * Response: index.ejs
+ * Response: index.ejs, Employee[]
  * URL: localhost:8080
  */
 app.get("/", (req, res) => {
   Employee.find({}, (err, employees) => {
-    User.findById(employees.user, (err, user) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("index", {
-          title: "EMS | Employees",
-          employees,
-          user
-        });
-      }
-    });
+    if (err) {
+      console.log(err);
+      throw err;
+    } else {
+      console.log(employees);
+      res.render("index", {
+        title: "EMS | Home",
+        employees
+      });
+    }
   });
 });
 
 // Route files
 let employees = require("./routes/employees");
-let users = require("./routes/users");
 app.use("/employees", employees);
-app.use("/users", users);
-
-// // GET: List all Employees
-// app.get("/employees", (req, res) => {
-//   res.render("list", {
-//     title: "Employee List"
-//   });
-// });
-
-// // POST: Add Ne Employee
-// app.get("/employees/new", (req, res) => {
-//   res.render("new", {
-//     title: "Add New Employee"
-//   });
-// });
-
-// // GET: View an Employee by id
-// app.get("/employee/:id", (req, res) => {
-//   res.render("view", {
-//     title: `Employee Details for first-name last-name`
-//   });
-// });
 
 // Start Express server
 app.listen(app.get("port"), () => {
--- ems/routes/employees.js
+++ ems/routes/employees.js
@@ -10,126 +10,158 @@ const router = express.Router();
 
 // Employee Model
 let Employee = require("../models/employee");
-// User Model
-let User = require("../models/user");
 
-// Add New Employee Route
-router.get("/new", ensureAuthenticated, (req, res) => {
+/**
+ * Description: Redirects users to the 'new' page
+ * Type: GET
+ * Request: n/a
+ * Response: new.ejs
+ * URL: localhost:8080/new
+ */
+router.get("/new", (req, res) => {
   res.render("new", {
-    title: "Add New Employee"
+    title: "EMS | New"
   });
 });
 
-// Add New Employee Process
-router.post("/new", (req, res) => {
-  req.checkBody("fName", "First Name is a required field!").notEmpty();
+/**
+ * Description: Processes form submission
+ * Type: POST
+ * Request: fName, lName, phone[], email
+ * Response: index.ejs
+ * URL: localhost:8080/process
+ */
+router.post("/process", (req, res) => {
+  if (!req.body.fName) {
+    res.status(400).send("Employee must have a First Name!");
+  }
+  if (!req.body.lName) {
+    res.status(400).send("Employee must have a Last Name!");
+  }
 
-  // Get errors
-  let errors = req.validationErrors();
+  // get the request's form data
+  const fName = req.body.fName;
+  const lName = req.body.lName;
+  const phone = [req.body.work, req.body.mobile, req.body.home];
+  const email = req.body.email;
 
-  if (errors) {
-    res.render("new", {
-      title: "Add New Employee",
-      errors
-    });
-  } else {
-    let employee = new Employee();
-    employee.user = req.user._id;
-    employee.fName = req.body.fName;
-    employee.lName = req.body.lName;
-    employee.phone = [req.body.work, req.body.mobile, req.body.home];
-    employee.email = req.body.email;
+  // create a employee model
+  let employee = new Employee({
+    fName,
+    lName,
+    phone,
+    email
+  });
 
-    employee.save(err => {
-      if (err) {
-        console.error(err);
-        return;
+  // save new employee record to the database
+  employee.save(err => {
+    if (err) {
+      console.log(err);
+      throw err;
+    } else {
+      console.log(`Employee: ${fName} ${lName} has been successfully saved!`);
+      res.redirect("/");
+    }
+  });
+});
+
+/**
+ * Description: Redirects users to the home page
+ * Type: GET
+ * Request: query
+ * Response: view.ejs, Employee[] | index.ejs
+ * URL: localhost:8080/view/:query
+ */
+router.get("/view/:query", (req, res) => {
+  const query = req.params["query"];
+
+  Employee.find({ name: query }, (err, employees) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    } else {
+      console.log(employees);
+
+      if (employees.length > 0) {
+        res.render("view", {
+          title: "EMS | View",
+          employee: employees
+        });
       } else {
-        req.flash("success", "New Employee Added");
         res.redirect("/");
       }
-    });
-  }
+    }
+  });
 });
 
 // Load Edit Form
-router.get("/edit/:id", ensureAuthenticated, (req, res) => {
-  Employee.findById(req.params.id, (err, employee) => {
-    if (employee.user != req.user._id) {
-      req.flash("danger", "Not Authorized!");
-      res.redirect("/");
-    }
-    res.render("edit", {
-      title: `Edit ${employee.fName} ${employee.lName}`,
-      employee
-    });
-  });
-});
+// router.get("/edit/:id", ensureAuthenticated, (req, res) => {
+//   Employee.findById(req.params.id, (err, employee) => {
+//     if (employee.user != req.user._id) {
+//       req.flash("danger", "Not Authorized!");
+//       res.redirect("/");
+//     }
+//     res.render("edit", {
+//       title: `Edit ${employee.fName} ${employee.lName}`,
+//       employee
+//     });
+//   });
+// });
 
 // Update Employee Process
-router.post("/edit/:id", (req, res) => {
-  let employee = {};
-  employee.fName = req.body.fName;
-  employee.lName = req.body.lName;
-  employee.phone = [req.body.work, req.body.mobile, req.body.home];
-  employee.email = req.body.email;
+// router.post("/edit/:id", (req, res) => {
+//   let employee = {};
+//   employee.fName = req.body.fName;
+//   employee.lName = req.body.lName;
+//   employee.phone = [req.body.work, req.body.mobile, req.body.home];
+//   employee.email = req.body.email;
 
-  let query = { _id: req.params.id };
+//   let query = { _id: req.params.id };
 
-  Employee.update(query, employee, err => {
-    if (err) {
-      console.error(err);
-      return;
-    } else {
-      req.flash("success", "Employee updated!");
-      req.redirect("/");
-    }
-  });
-});
+//   Employee.update(query, employee, err => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     } else {
+//       req.flash("success", "Employee updated!");
+//       req.redirect("/");
+//     }
+//   });
+// });
 
 // Delete Employee
-router.delete("/:id", (req, res) => {
-  if (!req.user._id) {
-    res.status(500).send();
-    console.log("Bad User ID!");
-  }
+// router.delete("/:id", (req, res) => {
+//   if (!req.user._id) {
+//     res.status(500).send();
+//     console.log("Bad User ID!");
+//   }
 
-  let query = { _id: req.params.id };
+//   let query = { _id: req.params.id };
 
-  Employee.findById(req.params.id, (err, employee) => {
-    if (employee.user != req.user._id) {
-      res.status(500).send();
-    } else {
-      Employee.remove(query, err => {
-        if (err) {
-          console.error(err);
-        }
-        res.send("Success");
-      });
-    }
-  });
-});
+//   Employee.findById(req.params.id, (err, employee) => {
+//     if (employee.user != req.user._id) {
+//       res.status(500).send();
+//     } else {
+//       Employee.remove(query, err => {
+//         if (err) {
+//           console.error(err);
+//         }
+//         res.send("Success");
+//       });
+//     }
+//   });
+// });
 
 // Get Single Employee
-router.get("/:id", (req, res) => {
-  Employee.findById(req.params.id, (err, employee) => {
-    User.findById(employee.user, (err, user) => {
-      res.render("view", {
-        title: `${employee.fName} ${employee.lName}`,
-        employee
-      });
-    });
-  });
-});
-
-// Access Control
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  } else {
-    req.flash("danger", "Please Login First!");
-    res.redirect("/users/login");
-  }
-}
+// router.get("/:id", (req, res) => {
+//   Employee.findById(req.params.id, (err, employee) => {
+//     User.findById(employee.user, (err, user) => {
+//       res.render("view", {
+//         title: `${employee.fName} ${employee.lName}`,
+//         employee
+//       });
+//     });
+//   });
+// });
 
 module.exports = router;
